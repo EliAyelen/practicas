dab9646f37029679e8a91c3c2ece6b88fa6d03c5
EliAyelen <eluchigimenez@gmail.com>
2024-05-21 21:07:32 -0300

    primmer commit

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+nro1=int(input("ingresar nro1 "))
+nro2=int(input("ingresar nro2 "))
+if nro1>nro2:
+    print(nro1)
+else:
+    print(nro2)    
+    
---
+++ nose.py
@@ -0,0 +1,7 @@
+nro1=int(input("ingresar nro 1 "))
+nro2=int(input("ingresar nro 2 "))
+nro3=int(input("ingresar nro 3 "))
+if nro1<nro2 and nro2<nro3:
+    print("es ascendente")
+else:
+    print("no es ascendente")
